5bdd0b32bb8cdc73de28a679e4441f75c378c80a
anya andrews <2019120080@ub.edu.bz>
2025-11-14 22:04:47 -0600

    Main

--- Lab3.cpp
+++ Lab3.cpp
@@ -3,8 +3,35 @@
 #include <climits>
 using namespace std;
 
+void createAdjacencyMatrix();
+void createAdjacencyList();
+void displayMenu();
+
 int main() {
-    
+
+    int choice;
+   
+    do {
+        displayMenu();
+        cout << "Enter your choice (1, 2, or 3): ";
+        cin >> choice;
+
+        switch(choice) {
+            case 1:
+                createAdjacencyMatrix();
+                break;
+            case 2:
+                createAdjacencyList();
+                break;
+            case 3:
+                cout << "Exiting program. Goodbye!" << endl;
+                break;
+            default:
+                cout << "Invalid choice! Please try again." << endl;
+        }
+        cout << endl;
+    } while (choice != 3);
+  
     return 0;
 
 }    
